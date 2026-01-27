86fbc54176acfb8ad4e6f0ca6cbc71485316145f
Neet2516 <getneet.25@gmail.com>
2026-01-27 16:01:37 +0530

    Added the Login Auth in the frontend

--- FRONTEND/src/pages/Login.jsx
+++ FRONTEND/src/pages/Login.jsx
@@ -4,13 +4,35 @@ import google from  '../assets/google.jpg'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { useNavigate }  from "react-router-dom"
+import axios from 'axios';
+import { serverUrl } from '../App';
+import { toast } from 'react-toastify';
 const Login = () => {
   const [show,setShow]= useState(false);
   const navigate = useNavigate() ;
+  const [email,setEmail]= useState("");
+  const [password , setPassword]=useState("");
+  const [loading,setLoading]=useState(false); 
+
+  const handleLogin  = async  ()=>{
+    setLoading(true)
+    try{
+      const result  = await axios.post(serverUrl+"/auth/login",{email,password},{withCredentials:true});
+      console.log(result.data);
+      toast.success("Login Successfully")
+      setLoading(false)
+    }catch(error){
+      console.log(`error is  ${error}`) ;
+      setLoading(false);
+      toast.error(error.response.data.message)
+    }
+  }
 
     return (
       <div className='bg-[#dddddb] w-screen h-screen flex items-center justify-center'>
-        <form className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex'>
+        <form className='w-[90%] md:w-200 h-150 bg-white shadow-xl rounded-2xl flex' onSubmit={(e)=>{
+          e.preventDefault() ;
+        }} >
           {/* leftdiv */}
           <div className=' mmd:w-[50%] w-full h-full flex flex-col items-center justify-center gap-3'>
             <div>
@@ -20,13 +42,14 @@ const Login = () => {
             
             <div className=' flex  flex-col gap-1 w-[80%] items-start justify-center  px-3'>
               <label htmlFor='email'  className='font-semibold'>Email</label>
-              <input id="email" type="text" className='border w-full h-8.75 border-[#e7e6e6] text-[15px]  px-5' placeholder="Your Email"/>
+              <input id="email" type="text" className='border w-full h-8.75 border-[#e7e6e6] text-[15px]  px-5' placeholder="Your Email" onChange={(e) => setEmail(e.target.value)}/>
   
             </div>
             <div className=' relative flex  flex-col gap-1 w-[80%] items-start justify-center  px-3'>
               <label htmlFor='password'  className='font-semibold'>Password</label>
               <input id="password" type={show?"text": "password"} className='border w-full h-[35px] border-[#e7e6e6] text-[15px]  px-[20px]' placeholder="Your Password"/>
-              {show ?<FaEye className=' absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onClick={()=>{
+              {show ?<FaEye className=' absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]' onChange={(e) => setPassword(e.target.value)}
+              onClick={()=>{
                 setShow(prev=>!prev)
               }}/>:
               <FaEyeSlash className=' absolute w-[20px] h-[20px] cursor-pointer right-[5%] bottom-[10%]'onClick={()=>{
@@ -34,7 +57,7 @@ const Login = () => {
               }}/>}
             </div>
             
-            <button className='w-[80%]  h-[40px]  text-white bg-black  cursor-pointer items-center justify-center rounded-[5px]'>Login</button>
+            <button className='w-[80%]  h-[40px]  text-white bg-black  cursor-pointer items-center justify-center rounded-[5px]' disabled={loading} onClick={handleLogin}>{loading?<ClipLoader size={30} color:white />:"Login"}</button>
             <span className='text-[13px] cursor-pointer text-[#585787] '>Forget your password</span>
             <div className='w-[80%] flex items-center gap-2 '>
               <div className='w-[25%] h-[0.5px] bg-[#c4c4c4]'>
